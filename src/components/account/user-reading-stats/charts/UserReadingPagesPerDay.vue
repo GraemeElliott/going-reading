7c9a5b21e2356051f8b284693c65e4ae5a3355db
Graeme Elliott <graeme.elliott@dimedout.com>
2025-01-19 14:42:17 +0000

    Dark mode styling added to the chart filters

--- src/components/account/user-reading-stats/charts/UserReadingPagesPerDay.vue
+++ src/components/account/user-reading-stats/charts/UserReadingPagesPerDay.vue
@@ -275,7 +275,11 @@ onMounted(() => {
     <div class="flex justify-end">
       <select
         v-model="viewMode"
-        class="block rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-800 dark:text-gray-100 dark:ring-gray-700"
+        class="block rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        :class="{
+          'bg-white text-black': !darkModeStore.darkMode,
+          'bg-gray-800 text-white': darkModeStore.darkMode,
+        }"
       >
         <option value="daily">Daily View</option>
         <option value="weekly">Weekly View</option>
